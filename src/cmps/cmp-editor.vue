3aa7e10ccac4bad31d760123c561ccecfb928e4d
Avishai Dotan <avishaiDotan@gmail.com>
2022-11-29 22:48:29 +0200

    first cmp editor wireing up

---
+++ src/cmps/cmp-editor.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="cmp-editor">
+        <font-picker v-if=""/>
+        
+    </section>
+</template>
+
+<script>
+    import {eventBus} from '../services/event-bus.service'
+    import fontPicker from './editor-cmps/font-picker.vue'
+
+    export default {
+        computed() {
+            return ['font', 'backgroundColor', 'fontWeight', 'img', 'href', 'borderRadius']
+        },
+        methods: {
+            isContain()
+        },
+        components: {
+            fontPicker, 
+        }
+    }
+</script>
