dc3d515828b72d81b282a02c903887d5556ad852
Diego Mc <projkd1@gmail.com>
2022-11-30 14:04:36 +0200

    bugfix

--- src/cmps/app-cmps/cmp-editor.vue
+++ src/cmps/app-cmps/cmp-editor.vue
@@ -1,63 +1,60 @@
 <template>
-    <section class="cmp-editor">
-        <section class="style-editor">
+  <section v-if="options" class="cmp-editor">
+    <section class="style-editor">
+      <div v-if="isOptionsContain('fontFamily')">
+        Font Picker
+        <!-- Implement v-model=style.fontFamily -->
+      </div>
 
-            <div v-if="isOptionsContain('fontFamily')">
-                Font Picker
-                <!-- Implement v-model=style.fontFamily -->
-            </div>
+      <div v-if="isOptionsContain('backgroundColor')">
+        Background Color Picker
+        <!-- Implement v-model=style.fontFamily -->
+      </div>
 
-            <div v-if="isOptionsContain('backgroundColor')">
-                Background Color Picker
-                <!-- Implement v-model=style.fontFamily -->
-            </div>
+      <div v-if="isOptionsContain('color')">
+        Color Picker
+        <!-- Implement v-model=style.color -->
+      </div>
 
-            <div v-if="isOptionsContain('color')">
-                Color Picker
-                <!-- Implement v-model=style.color -->
-            </div>
+      <div v-if="isOptionsContain('fontWeight')">
+        Font Weight Picker
+        <!-- Implement v-model=style.fontWeight -->
+      </div>
 
-            <div v-if="isOptionsContain('fontWeight')">
-                Font Weight Picker
-                <!-- Implement v-model=style.fontWeight -->
-            </div>
-
-            <div v-if="isOptionsContain('borderRadius')">
-                Border Radius Picker
-                <!-- Implement v-model=style.fontWeight -->
-            </div>
-
-        </section>
-
-        <section class="content-editor">
-
-            <div v-if="isOptionsContain('img')">
-                Img Picker
-                <!-- Implement v-model=style.img -->
-            </div>
-
-            <div v-if="isOptionsContain('link')">
-                Link
-                <!-- Implement v-model=style.link -->
-            </div>
-
-        </section>
-        
+      <div v-if="isOptionsContain('borderRadius')">
+        Border Radius Picker
+        <!-- Implement v-model=style.fontWeight -->
+      </div>
     </section>
+
+    <section class="content-editor">
+      <div v-if="isOptionsContain('img')">
+        Img Picker
+        <!-- Implement v-model=style.img -->
+      </div>
+
+      <div v-if="isOptionsContain('link')">
+        Link
+        <!-- Implement v-model=style.link -->
+      </div>
+    </section>
+  </section>
 </template>
 
 <script>
-
-    export default {
-        props: {
-            id: String,
-            options: Object,
-        },
-        methods: {
-            isOptionsContain(type) {
-                const options = [...Object.keys(this.options)]
-                return options.includes(type)
-            },
-        },
-    }
+export default {
+  props: {
+    id: String,
+    options: Object,
+  },
+  methods: {
+    isOptionsContain(type) {
+      const options = [...Object.keys(this.options)]
+      return options.includes(type)
+    },
+  },
+  created() {
+    console.log(this.options, 'FDHDF')
+  },
+}
 </script>
