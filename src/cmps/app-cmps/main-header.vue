ea0a6238227d78e09e0ecb38ace7dc9b703bc761
Diego Mc <projkd1@gmail.com>
2022-12-01 14:41:48 +0200

    bugfix

--- src/cmps/app-cmps/main-header.vue
+++ src/cmps/app-cmps/main-header.vue
@@ -1,5 +1,24 @@
 <template>
   <header class="main-header">
+    <section class="edit-options">
+      <section class="history-options">
+        <!-- <i class="icon undo-btn bi bi-arrow-counterclockwise"></i> -->
+        <!-- <i class="icon redo-btn bi bi-arrow-clockwise"></i> -->
+        <editor-btn-group
+          gap="4px"
+          info="media"
+          :opts="[
+            { val: 'undo', icon: 'arrow-counterclockwise' },
+            { val: 'redo', icon: 'arrow-clockwise' },
+          ]" />
+      </section>
+      <section class="work-together">
+        <editor-btn-group
+          gap="4px"
+          info="media"
+          :opts="[{ val: 'undo', icon: 'people' }]" />
+      </section>
+    </section>
     <div class="main-logo">
       <img class="logo" src="src/assets/imgs/wewix-logo.svg" />
       <h1 class="logo-text">wewix</h1>
@@ -9,7 +28,12 @@
 </template>
 
 <script>
-export default {}
+import editorBtnGroup from './editor-btn-group.vue'
+export default {
+  components: {
+    editorBtnGroup,
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
