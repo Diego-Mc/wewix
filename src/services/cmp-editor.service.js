b73c78433a8160cb2b55a1519a4a250d416a50d4
Avishai Dotan <avishaiDotan@gmail.com>
2022-12-02 13:23:04 +0200

    minor fix on map feat

--- src/services/cmp-editor.service.js
+++ src/services/cmp-editor.service.js
@@ -5,18 +5,29 @@ export const cmpEditorService = {
 }
 
 async function getMapData(locationName) {
+    if (!locationName) {
+        return {
+            title: '',
+            label: '',
+            position: {
+                lat: '',
+                lng: '',
+            },
+        }
+    }
+    
     const api_key = 'pk.dc87f5d9af931a664e3281457d48045b'
     try {
-        const {data} = await axios.get(`https://eu1.locationiq.com/v1/search?key=${api_key}&q=${locationName}&format=json`)
+        const { data } = await axios.get(`https://eu1.locationiq.com/v1/search?key=${api_key}&q=${locationName}&format=json`)
 
         const location = data[0]
 
-        const mapData = (location) ? 
-              {title: location.display_name, label: location.display_name.substring(0, 3).toUpperCase(),  position: {lat: +location.lat, lng: +location.lon}} :
-              {title: 'Jerusalem', label: 'JSM', position: { lat: 31.7683, lng: 35.2137 }}
-        
+        const mapData = (location) ?
+            { title: location.display_name, label: location.display_name.substring(0, 3).toUpperCase(), position: { lat: +location.lat, lng: +location.lon } } :
+            { title: 'Jerusalem', label: 'JSM', position: { lat: 31.7683, lng: 35.2137 } }
+
         return mapData
-    } catch(err) {
+    } catch (err) {
         throw new Error('Map hasnt been loaded', err)
     }
 
