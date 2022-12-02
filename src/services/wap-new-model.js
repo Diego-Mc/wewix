a561331f21972df342fc52b6f469285b29d13565
Diego Mc <projkd1@gmail.com>
2022-12-02 09:45:29 +0200

    Added map json example

--- src/services/wap-new-model.js
+++ src/services/wap-new-model.js
@@ -1,79 +1,85 @@
 import getCmp from './wap-cmps.service'
 export const wapVideo = {
-    id: 'wc10',
-    type: 'wap-video',
-    options: {
-      meta: {
-        img: '',
-        link: 'https://www.youtube.com/embed/sfabsNX61aU',
-      },
-      style: {
-        backgroundColor: '',
-        fontFamily: '',
-        color: '',
-        borderRadius: '2em',
-        fontWeight: '',
-      },
+  id: 'wc10',
+  type: 'wap-video',
+  options: {
+    meta: {
+      img: '',
+      link: 'https://www.youtube.com/embed/sfabsNX61aU',
     },
-    info: {
-      title: {
-        options: {
-          meta: {},
-          style: {},
-        },
-        content: {},
-      },
+    style: {
+      backgroundColor: '',
+      fontFamily: '',
+      color: '',
+      borderRadius: '2em',
+      fontWeight: '',
     },
+  },
+  info: {
+    title: {
+      options: {
+        meta: {},
+        style: {},
+      },
+      content: {},
+    },
+  },
 }
 
 export const wapMap = {
-    id: 'wc10',
-    type: 'wap-map',
-    options: {
-      meta: {
-        img: '',
-        link: '',
-      },
-      style: {
-        backgroundColor: '',
-        fontFamily: '',
-        color: '',
-        borderRadius: '',
-        fontWeight: '',
+  id: 'wc10',
+  type: 'wap-map',
+  options: {
+    meta: {
+      latLng: {
+        lat: '',
+        lng: '',
       },
     },
-    info: {
-      title: {
-        options: {
-          meta: {},
-          style: {},
-        },
-        content: {},
-      },
+    style: {
+      backgroundColor: '',
+      fontFamily: '',
+      color: '',
+      borderRadius: '',
+      fontWeight: '',
     },
+  },
+  info: {
+    title: {
+      options: {
+        meta: {},
+        style: {},
+      },
+      content: {},
+    },
+  },
 }
 
 export default [
-    {
-        _id: '5e28393890dd7201a06d4e44',
-        name: 'HairDresser Baluta Marketing Site',
-        imgUrl:
-            'http://res.cloudinary.com/webify/image/upload/v1580021948/coffe_yi0yzf.png',
-        createdBy: {
-            _id: '5e26e0b718a0891d4c995527',
-            fullname: 'Hekro Special',
-            imgUrl: 'img.jpg',
-        },
-        usersData: {
-            contacts: [
-                { email: 'user@user.com', msg: 'Please send me stuff', at: 123 },
-            ],
-            subscriptions: [{ email: 'user@user.com', at: 123 }],
-        },
-        isOnline: false,
-        cmps: [getCmp('wap-header', 1), getCmp('wap-hero', 1), getCmp('wap-section', 1), getCmp('wap-cards', 1), wapVideo, wapMap ],
-    }
-
+  {
+    _id: '5e28393890dd7201a06d4e44',
+    name: 'HairDresser Baluta Marketing Site',
+    imgUrl:
+      'http://res.cloudinary.com/webify/image/upload/v1580021948/coffe_yi0yzf.png',
+    createdBy: {
+      _id: '5e26e0b718a0891d4c995527',
+      fullname: 'Hekro Special',
+      imgUrl: 'img.jpg',
+    },
+    usersData: {
+      contacts: [
+        { email: 'user@user.com', msg: 'Please send me stuff', at: 123 },
+      ],
+      subscriptions: [{ email: 'user@user.com', at: 123 }],
+    },
+    isOnline: false,
+    cmps: [
+      getCmp('wap-header', 1),
+      getCmp('wap-hero', 1),
+      getCmp('wap-section', 1),
+      getCmp('wap-cards', 1),
+      wapVideo,
+      wapMap,
+    ],
+  },
 ]
-
-
