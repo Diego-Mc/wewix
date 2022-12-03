d49d7c2adeafa67255099959f87223354307c106
Diego Mc <projkd1@gmail.com>
2022-12-03 18:28:32 +0200

    quick change

--- src/services/wap-cmps.service.js
+++ src/services/wap-cmps.service.js
@@ -135,18 +135,6 @@ function getStyles(elType) {
   }
 }
 
-const header1 = {
-  previewUrl: '',
-  typeId: 1,
-  type: 'header',
-}
-const header1otherOption = {
-  previewUrl: '',
-  typeId: 1,
-  type: 'header',
-  'wap-header-1': '#...',
-}
-
 const wapsMap = new Map()
 wapsMap.set('wap-header-1', {
   id: '',
