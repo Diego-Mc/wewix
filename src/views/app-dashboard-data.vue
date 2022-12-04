b21653ad12f31decf47d1dee9e9beb27b3caa2b3
Nir <nircoren@gmail.com>
2022-12-05 01:51:25 +0200

    styled dashboard

--- src/views/app-dashboard-data.vue
+++ src/views/app-dashboard-data.vue
@@ -1,42 +1,92 @@
 <template>
-    <div>
-        <app-dashboard-graphs :usersData="getDemoData"/>
-        <app-dashboard-details :usersData="getDemoData"/>
+  <section class="dashboard-data">
+    <div class="dashboard-top-div">
+      <h2>Kite Shop's Dashboard</h2>
+      <div>
+        <router-link :to="''">
+          <el-button class="view-wap-btn">View Website</el-button>
+        </router-link>
+        <router-link :to="''">
+          <button class="edit-wap-btn">Edit Website</button>
+        </router-link>
+      </div>
     </div>
+    <main class="graph-and-stats-container">
+      <app-dashboard-graphs :usersData="getDemoData" />
+      <app-dashboard-stats />
+    </main>
+    <!-- <app-dashboard-details :usersData="getDemoData" /> -->
+  </section>
 </template>
 
 <script>
-import Chance from 'chance';
-import appDashboardGraphs from '../cmps/app-cmps/app-dashboard-graphs.vue';
-import appDashboardDetails from '../cmps/app-cmps/app-dashboard-details.vue';
+import Chance from 'chance'
+import appDashboardGraphs from '../cmps/app-cmps/app-dashboard-graphs.vue'
+import appDashboardDetails from '../cmps/app-cmps/app-dashboard-details.vue'
+import appDashboardStats from '../cmps/app-cmps/app-dashboard-stats.vue'
 
 export default {
-    computed: {
-        getDemoData() {
-        var chance = new Chance()
-        const demoData = []
-        for (var i = 0; i < 1500; i++) {
-          demoData.push({
-            firstName: chance.name().split(' ')[0],
-            lastName: chance.name().split(' ')[0],
-            email: chance.email(),
-            createdAt: chance
-              .date({ year: chance.integer({ min: 2020, max: 2022 }) })
-              [Symbol.toPrimitive]('number'),
-            msg: chance.sentence({ words: 5 }),
-          })
-        //   if (Math.random() < 0.1) demoData[i].addLike = true
-        //   else if (Math.random() < 0.3) demoData[i].isSubscribed = true
-        //   else if (Math.random() < 0.5) demoData[i].gender = (Math.random() < 0.5) ? 'male' : 'female'
-        //   else if (Math.random() < 0.8) demoData[i][(Math.random() < 0.5) ? 'isJobAssign' : 'isHobbyAssign'] = (Math.random() < 0.5)
-        }
-        console.log(demoData);
-        return demoData
-      },
+  computed: {
+    getDemoData() {
+      var chance = new Chance()
+      const demoData = []
+      for (var i = 0; i < 1500; i++) {
+        demoData.push({
+          firstName: chance.name().split(' ')[0],
+          lastName: chance.name().split(' ')[0],
+          email: chance.email(),
+          createdAt: chance
+            .date({ year: chance.integer({ min: 2020, max: 2022 }) })
+            [Symbol.toPrimitive]('number'),
+          msg: chance.sentence({ words: 5 }),
+        })
+        if (Math.random() < 0.1) demoData[i].addLike = true
+        else if (Math.random() < 0.3) demoData[i].isSubscribed = true
+        else if (Math.random() < 0.5)
+          demoData[i].gender = Math.random() < 0.5 ? 'male' : 'female'
+        else if (Math.random() < 0.8)
+          demoData[i][Math.random() < 0.5 ? 'isJobAssign' : 'isHobbyAssign'] =
+            Math.random() < 0.5
+      }
+      console.log(demoData)
+      return demoData
     },
-    components: {
+  },
+  components: {
     appDashboardGraphs,
-    appDashboardDetails
-}
+    appDashboardDetails,
+    appDashboardStats,
+  },
 }
 </script>
+
+<style lang="scss">
+.dashboard-data {
+  padding: 25px 15px 100px 25px;
+}
+.dashboard-top-div {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 53px;
+  button {
+    border-radius: 50px;
+  }
+  .edit-wap-btn {
+    background: rgba(0, 194, 166, 0.5);
+    border: 1px solid #eeeeee;
+    color: #ffffff;
+  }
+  .view-wap-btn {
+    background: #ffffff;
+    border: 1px solid #00c2a6;
+    color: #00c2a6;
+    margin-right: 15px;
+  }
+}
+
+.graph-and-stats-container{
+  display: flex;
+  justify-content: space-between;
+  
+}
+</style>
