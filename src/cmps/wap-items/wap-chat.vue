b605ddfab3dc7c694b31ce754739cec20287b3ae
Diego Mc <projkd1@gmail.com>
2022-12-07 21:49:02 +0200

    Started designing chat

--- src/cmps/wap-items/wap-chat.vue
+++ src/cmps/wap-items/wap-chat.vue
@@ -1,17 +1,26 @@
 <template>
-  <button @click="(isChatOpen = !isChatOpen)">Click Here To Open Chat</button>
+  <button @click="isChatOpen = !isChatOpen">Click Here To Open Chat</button>
 
   <section class="wap-chat" v-if="isChatOpen">
     <header>
-      <nav>
+      <!-- <nav>
         <ul>
-          <li>
-            👧👸👨‍🦱
-          </li>
-          <li>
-            ®©®
-          </li>
+          <li>👧👸👨‍🦱</li>
+          <li>®©®</li>
         </ul>
+      </nav> -->
+      <nav class="guests">
+        <span
+          class="guest"
+          v-for="u in activeUsers"
+          :key="u"
+          @click="setActiveConversation(u)">
+          <!-- Todo Make It Random -->
+          <img
+            class="guest-avatar"
+            src="../../assets/imgs/png-96/avatar1.png" />
+          <span>{{ u.unread }}</span>
+        </span>
       </nav>
       <div class="welcome-txt">
         <h1>Hey! 👋</h1>
@@ -19,10 +28,11 @@
       </div>
     </header>
 
-
     <hr />
     <ul>
-      <li v-for="(msg, idx) in conversations[this.activeConversation]" :key="idx">
+      <li
+        v-for="(msg, idx) in conversations[this.activeConversation]"
+        :key="idx">
         <span>{{ msg.from }}:</span>{{ msg.txt }}
       </li>
     </ul>
@@ -30,12 +40,6 @@
       <input type="text" v-model="msg.txt" placeholder="Your msg" />
       <button>Send</button>
     </form>
-    
-    <span v-for="u in activeUsers" :key="u" @click="setActiveConversation(u)">
-      <!-- Todo Make It Random -->
-      <img class="guest-avatar" src="../../assets/imgs/png-96/avatar1.png">
-      <span>{{u.unread}}</span>
-    </span>
   </section>
 </template>
 
@@ -47,16 +51,15 @@ import {
 } from '../../services/socket.service'
 import { utilService } from '../../services/util.service'
 
-
 export default {
   props: {
-    options: Object
+    options: Object,
   },
   data() {
     return {
-      msg: {txt: '' },
+      msg: { txt: '' },
       msgs: [],
-      isChatOpen: false,
+      isChatOpen: true, //TODO: change to false
 
       conversations: {},
       activeUsers: [],
@@ -67,27 +70,28 @@ export default {
   },
   created() {
     socketService.emit('startConversation', {
-      chatId: this.chatId, 
-      userId: this.user.id, 
-      adminId: (this.user.isAdmin) ? this.user.id : ''
+      chatId: this.chatId,
+      userId: this.user.id,
+      adminId: this.user.isAdmin ? this.user.id : '',
     })
 
     socketService.on('emitToAdmin', (activeUsers) => {
-        if (!this.user.isAdmin) return
+      if (!this.user.isAdmin) return
 
-        this.activeUsers = activeUsers
-        socketService.emit('listenAll', {})
+      this.activeUsers = activeUsers
+      socketService.emit('listenAll', {})
     })
 
     socketService.on('addMsg', this.addMsg)
 
     socketService.on('setGuestActiveConversation', (userId) => {
-        if (this.user.id !== userId) return
+      if (this.user.id !== userId) return
 
-        const user = this.activeUsers.find(({activeUser}) => activeUser.userId === userId)
-        this.activeConversation = userId
+      const user = this.activeUsers.find(
+        ({ activeUser }) => activeUser.userId === userId
+      )
+      this.activeConversation = userId
     })
-
   },
   unmounted() {
     socketService.off(SOCKET_EVENT_ADD_MSG, this.addMsg)
@@ -98,11 +102,14 @@ export default {
       if (this.conversations[msg.id]) this.conversations[msg.id].push(msg)
       else this.conversations[msg.id] = [msg]
 
-      const user = this.activeUsers.find(({userId}) => userId === msg.id)
-      
-      console.log('this.activeConversation, user:', this.activeConversation, user)
-      if (user && (this.activeConversation !== user.userId)) user.unread++
+      const user = this.activeUsers.find(({ userId }) => userId === msg.id)
 
+      console.log(
+        'this.activeConversation, user:',
+        this.activeConversation,
+        user
+      )
+      if (user && this.activeConversation !== user.userId) user.unread++
     },
     sendMsg() {
       this.msg.from = this.user.userNickname
@@ -110,56 +117,30 @@ export default {
       this.msg = { txt: '' }
     },
     getUser() {
-        const user = JSON.parse(JSON.stringify(this.$store.getters.loggedinUser))
+      const user = JSON.parse(JSON.stringify(this.$store.getters.loggedinUser))
 
-        if (!user) return {nickname: 'guest', id: Math.random() + ''}
+      if (!user) return { nickname: 'guest', id: Math.random() + '' }
 
-        user.isAdmin = true
-        user.nickname = 'Admin'
-        user.id = user._id
+      user.isAdmin = true
+      user.nickname = 'Admin'
+      user.id = user._id
 
-        delete user._id
-        console.log('user:', user)
-        return user       
+      delete user._id
+      console.log('user:', user)
+      return user
     },
     setActiveConversation(user) {
-        this.activeConversation = user.userId
-        user.unread = 0
-        socketService.emit('activateConversation', user.userId)
+      this.activeConversation = user.userId
+      user.unread = 0
+      socketService.emit('activateConversation', user.userId)
     },
-    avatar() { 
+    avatar() {
       return '../../assets/imgs/png-96/avatar1.png'
     },
   },
 
-  computed: {
-  }
-
+  computed: {},
 }
 </script>
 
-<style lang="scss" scoped>
-.guest-avatar {
-    height: 50px;
-}
-.wap-chat {
-  position: fixed;
-  bottom: 25px;
-  right: 25px;
-  height: 400px;
-  width: 350px;
-  background-color: lightblue;
-  padding: 10px;
-
-  nav ul {
-    display: flex;
-    justify-content: space-between;
-    margin-block: 0;
-    padding-inline: 0;
-  }
-
-  .welcome-txt {
-    margin-block-start: 50px;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
