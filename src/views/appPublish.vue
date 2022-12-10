299f19d1f56bbfcf94138584be4bfbb4be685b51
Avishai Dotan <avishaiDotan@gmail.com>
2022-12-11 01:48:25 +0200

    fix chat

--- src/views/appPublish.vue
+++ src/views/appPublish.vue
@@ -11,7 +11,7 @@
       :cmps="cmp.cmps" />
 
     <section v-if="wap?.plugins?.chatData">
-      <wap-chat :chatRoom="wap._id" />
+      <wap-chat :wapName="wap.name"/>
     </section>
 
     <section v-if="showErrPage">
